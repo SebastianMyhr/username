a73895d5a69c75369325191cf131e27c21583454
Sebastian Myhr <sebmyhr@gmail.com>
2020-11-15 20:53:15 -0800

    Adds username.sh

--- username.sh
+++ username.sh
@@ -1,12 +1,12 @@
-#! /bin/bash
-# zip.sh
-# author
-echo "Enter a five-digit ZIP code: "
+#/bin/bash
+# Username.sh
+# Sebastian Myhr
+echo "Enter a Username: "
 read -r NAME 
-while echo "$NAME" | grep -E -v "^[a-z][0-9]{5}$" > /dev/null 2>&1
+while echo "$NAME" | grep -E -v "^[a-z][0-9a-z_]{2,11}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid ZIP code - five digits only!"
-	echo "Enter a five-digit ZIP code: "
+	echo "You can only input lower case letters, digits, and the underscore character"
+	echo "Enter a Username: "
 	read -r NAME
 done
 echo "Thank you"
